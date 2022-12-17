4975bcd72597c001dbb9bdffb4383c7abcc9afef
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-12-17 13:50:08 +0100

    chore: comply with new design

--- skeletons/skeleton.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#title           :
-#description     :
-#author          :Carlos Vigil Vásquez
-#date            :
-#version         :
-#notes           :
-#copyright       :Copyright (C) 2022 Carlos Vigil Vásquez (cvigil2@uc.cl)
-#license         :Permission to copy and modify is granted under the MIT license
