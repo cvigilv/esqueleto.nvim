a97cda19673ece5276706b7cad0768464fa16728
Futar <Futarimiti@163.com>
2023-09-29 15:41:44 +0100

    chore: use more sensible approach for ignored pats
    also rename ignore-patterns constant

--- lua/esqueleto/constants.lua
+++ lua/esqueleto/constants.lua
@@ -2,6 +2,6 @@ local M = {}
 
 -- OS-specific ignoring files, from `https://www.toptal.com/developers/gitignore/api/windows,macos,linux`
 -- Index: Thu Sep 28 20:45:56 BST 2023
-M.ignored_files = { '*~', '.fuse_hidden*', '.directory', '.Trash-*', '.nfs*', '.DS_Store', '.AppleDouble', '.LSOverride', 'Icon', '._*', '.DocumentRevisions-V100', '.fseventsd', '.Spotlight-V100', '.TemporaryItems', '.Trashes', '.VolumeIcon.icns', '.com.apple.timemachine.donotpresent', '.AppleDB', '.AppleDesktop', 'Network Trash Folder', 'Temporary Items', '.apdisk', '*.icloud', 'Thumbs.db', 'Thumbs.db:encryptable', 'ehthumbs.db', 'ehthumbs_vista.db', '*.stackdump', '[Dd]esktop.ini', '$RECYCLE.BIN/', '*.cab', '*.msi', '*.msix', '*.msm', '*.msp', '*.lnk' }
+M.ignored_patterns = { '*~', '.fuse_hidden*', '.directory', '.Trash-*', '.nfs*', '.DS_Store', '.AppleDouble', '.LSOverride', 'Icon', '._*', '.DocumentRevisions-V100', '.fseventsd', '.Spotlight-V100', '.TemporaryItems', '.Trashes', '.VolumeIcon.icns', '.com.apple.timemachine.donotpresent', '.AppleDB', '.AppleDesktop', 'Network Trash Folder', 'Temporary Items', '.apdisk', '*.icloud', 'Thumbs.db', 'Thumbs.db:encryptable', 'ehthumbs.db', 'ehthumbs_vista.db', '*.stackdump', '[Dd]esktop.ini', '$RECYCLE.BIN/', '*.cab', '*.msi', '*.msix', '*.msm', '*.msp', '*.lnk' }
 
 return M
