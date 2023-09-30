725de63ce4905329eb470710db3493c3737d3b4e
Futar <Futarimiti@163.com>
2023-09-30 18:28:43 +0100

    ditto

--- lua/esqueleto/utils.lua
+++ lua/esqueleto/utils.lua
@@ -73,68 +73,6 @@ M.gettemplates = function(pattern, opts)
   return templates
 end
 
--- List ignored files under a directory, given a list of glob patterns
-local listignored = function(dir, ignored_pats)
-  return vim.tbl_flatten(vim.tbl_map(function(pat)
-    return vim.fn.globpath(dir, pat, true, true, true)
-  end, ignored_pats))
-end
-
--- Returns a ignore checker
-local getignorechecker = function(opts)
-  local os_ignore_pats = opts.use_os_ignore and require('esqueleto.constants').ignored_patterns or {}
-  local extra = opts.extra_ignore
-  local extra_ignore_pats, extra_ignore_func = (function()
-    if type(extra) == 'function' then
-      return {}, extra
-    else
-      assert(type(extra) == 'table')
-      return extra, function(_) return false end
-    end
-  end)()
-
-  return function(filepath)
-    local dir = vim.fn.fnamemodify(filepath, ':p:h')
-    return extra_ignore_func(dir) or vim.tbl_contains(listignored(dir, os_ignore_pats), filepath)
-      or vim.tbl_contains(listignored(dir, extra_ignore_pats), filepath)
-  end
-end
-
-M.getunignoredtemplates = function(pattern, opts)
-  local templates = {}
-  local isignored = getignorechecker(opts)
-
-  local alldirectories = vim.tbl_map(function(f)
-    return vim.fn.fnamemodify(f, ':p')
-  end, opts.directories)
-
-  -- Count directories that contain templates for pattern
-  local ndirs = 0
-  for _, directory in pairs(alldirectories) do
-    ndirs = ndirs + vim.fn.isdirectory(directory .. pattern .. '/')
-  end
-
-  -- Get templates for pattern
-  for _, directory in ipairs(alldirectories) do
-    local pattern_dir = directory .. pattern .. '/'
-    local exists_dir = vim.fn.isdirectory(pattern_dir) == 1
-    if exists_dir then
-      for basename in vim.fs.dir(pattern_dir) do
-        local filepath = vim.fs.normalize(pattern_dir .. basename)
-        if not isignored(filepath) then
-          local name = vim.fs.basename(filepath)
-          if ndirs > 1 then
-            name = vim.fn.simplify(directory) .. " :: " .. name
-          end
-          templates[name] = filepath
-        end
-      end
-    end
-  end
-
-  return templates
-end
-
 M.selecttemplate = function(templates, opts)
   -- Check if templates exist
   if vim.tbl_isempty(templates) then
@@ -182,11 +120,7 @@ M.inserttemplate = function(opts)
     end
 
     -- Get templates for selected pattern
-<<<<<<< HEAD
-    local templates = M.getunignoredtemplates(pattern, opts)
-=======
     local templates = M.gettemplates(pattern, opts)
->>>>>>> origin/main
 
     -- Pop-up selection UI
     M.selecttemplate(templates, opts)
