cb0d358efdf5c8de219cc3db4d08b835a406328d
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2024-02-29 00:12:07 -0300

    fix: don't prompt insertion on files with buftype equal to `nofile`
    Solves #46

--- lua/esqueleto/autocmd.lua
+++ lua/esqueleto/autocmd.lua
@@ -1,4 +1,4 @@
-local utils = require('esqueleto.utils')
+local utils = require("esqueleto.utils")
 
 local M = {}
 
@@ -6,25 +6,24 @@ local M = {}
 ---@param opts table Plugin configuration table
 M.createautocmd = function(opts)
   -- create autocommands for skeleton insertion
-  local group = vim.api.nvim_create_augroup(
-    "esqueleto",
-    { clear = true }
-  )
+  local group = vim.api.nvim_create_augroup("esqueleto", { clear = true })
 
-  vim.api.nvim_create_autocmd(
-    { "BufWinEnter", "BufReadPost", "FileType" },
-    {
-      group = group,
-      desc = "esqueleto.nvim :: Insert template",
-      pattern = opts.patterns,
-      callback = function()
-        local filepath = vim.fn.expand("%")
-        local emptyfile = vim.fn.getfsize(filepath) < 4
-        if emptyfile then utils.inserttemplate(opts) end
+  vim.api.nvim_create_autocmd({ "BufWinEnter", "BufReadPost", "FileType" }, {
+    group = group,
+    desc = "esqueleto.nvim :: Insert template",
+    pattern = opts.patterns,
+    callback = function()
+      if vim.bo.buftype == "nofile" then
+        return nil
       end
-    }
-  )
-end
 
+      local filepath = vim.fn.expand("%")
+      local emptyfile = vim.fn.getfsize(filepath) < 4
+      if emptyfile then
+        utils.inserttemplate(opts)
+      end
+    end,
+  })
+end
 
 return M
