9abe34c3e17c6f54e32cc4eaf203bb75b94fa263
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2024-11-25 21:42:03 +0100

    feat(log): add logging module

--- lua/esqueleto/init.lua
+++ lua/esqueleto/init.lua
@@ -1,3 +1,5 @@
+local log = require("esqueleto.log")
+
 _G.esqueleto_inserted = {}
 
 local M = {}
@@ -5,14 +7,17 @@ local M = {}
 --- Setup `esqueleto.nvim`
 ---@param opts Esqueleto.Config User configuration table
 M.setup = function(opts)
+  log.info("Load modules")
   local autocmd = require("esqueleto.autocmd")
   local config = require("esqueleto.config")
   local excmd = require("esqueleto.excmd")
 
   -- update defaults
+  log.info("Update configuration with user options")
   opts = config.update_config(opts)
 
   -- Module functionality
+  log.info("Create autocmds and excmds")
   autocmd.createautocmd(opts)
   excmd.createexcmd(opts)
 end
